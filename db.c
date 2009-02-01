f4b69018295e46a36b8f3f1863e4a234c667e695
Simon Shine <shreddedglory@gmail.com>
2009-02-01 15:38:44 +0100

    Broken code because of db_callbacks.c

--- db.c
+++ db.c
@@ -1,25 +1,5 @@
 #include <notez.h>
 
-static int notez_default_callback(void *not_used, int argc, char **argv, char **col_name)
-{
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
-    }
-    return 0;
-}
-
-static int notez_list_callback(void *not_used, int argc, char **argv, char **col_name)
-{
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
-    }
-    return 0;
-}
-
 NotezDB *notez_init(char *filename)
 {
     NotezDB *ndb;
---
+++ db_callbacks.c
@@ -0,0 +1,23 @@
+#include <notez.h>
+
+static int notez_default_callback(void *not_used, int argc, char **argv, char **col_name)
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
+    }
+    return 0;
+}
+
+static int notez_list_callback(void *not_used, int argc, char **argv, char **col_name)
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", col_name[i], argv[i] ? argv[i] : "NULL");
+    }
+    return 0;
+}
+
+
