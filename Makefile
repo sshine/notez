7ae40c5cb60a15f47a4d2bc0a7139db59cf266a2
Simon Shine <shreddedglory@gmail.com>
2009-02-05 01:34:16 +0100

    Improved Makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,19 @@
+CC = gcc
 CFLAGS = -Wall -O2
 INCLUDES = -I. -I/usr/include
 LIBS = -lsqlite3
+BIN = notez
 
-default:
-	gcc -o notez help.c duration.c db_callbacks.c db.c main.c \
-		$(CFLAGS) $(LIBS) $(INCLUDES)
+CFILES = help.c duration.c db_callbacks.c db.c main.c
+OBJS = ${CFILES:.c=.o}
+
+all: ${CFILES} ${BIN}
+
+${BIN}: ${OBJS}
+	${CC} ${CFLAGS} ${LIBS} ${OBJS} -o $@
+
+.c.o:
+	${CC} ${CFLAGS} ${INCLUDES} -c $< -o $@
+
+clean:
+	rm -f ${OBJS} ${BIN}
--- notez.h
+++ notez.h
@@ -8,8 +8,10 @@ typedef struct t_notez_db {
     sqlite3 *db;
 } NotezDB;
 
-#define NOTEZ_DB_INIT "CREATE TABLE IF NOT EXISTS notez (id integer primary key, title text, note text, changed datetime)"
-#define NOTEZ_DB_LIST_QUERY "SELECT id, strftime(\"%s\", changed) AS changed, title FROM notez ORDER BY id DESC"
+#define NOTEZ_DB_INIT "CREATE TABLE IF NOT EXISTS notez" \
+    " (id integer primary key, title text, note text, changed datetime)"
+#define NOTEZ_DB_LIST_QUERY "SELECT id, strftime(\"%s\", changed)" \
+    " AS changed, title FROM notez ORDER BY id DESC"
 
 NotezDB *notez_init(char *);
 void notez_list(NotezDB *);
